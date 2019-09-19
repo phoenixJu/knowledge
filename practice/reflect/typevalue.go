a761b3b1a7e477c2e5af25fa0c950c378498cb68
zhuhongquan <zhuhongquan@bytedance.com>
2019-09-19 11:41:01 +0800

    reflect

---
+++ practice/reflect/typevalue.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+
+func main()  {
+	// reflect中最基本的两个interface是Type和Value，分别通过TypeOf和ValueOf获得
+	// 获取的是实际类型或者值
+	a := 3888888888888888888
+	fmt.Println(reflect.TypeOf(a))
+    var x interface{}
+	fmt.Println(reflect.TypeOf(x))
+	fmt.Println(reflect.ValueOf(x))
+	type  Person struct{
+		Name string
+		Age int
+	}
+	p := &Person{"sprzhing", 33,}
+	fmt.Println(reflect.TypeOf(p))
+	fmt.Println(reflect.ValueOf(p))
+	p2 := Person{"qwj", 33,}
+	fmt.Println(reflect.TypeOf(p2))
+	fmt.Println(reflect.ValueOf(p2))
+	//
+	pi := reflect.ValueOf(p2).Interface()
+	fmt.Println(pi)
+}
