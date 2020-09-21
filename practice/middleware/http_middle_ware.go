185ad4ebf55bf9571e19f8af9b2aa8f09c8c20fb
zhuhongquan <zhuhongquan@bytedance.com>
2020-09-22 00:26:47 +0800

    [f_479491]add middleware

---
+++ practice/middleware/http_middle_ware.go
@@ -0,0 +1,48 @@
+package middleware
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+type MiddleWare func(handler http.HandlerFunc) http.HandlerFunc
+
+func Logging() MiddleWare {
+	return func(f http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+			defer func() { log.Println(r.URL.Path, time.Since(start)) }()
+			f(w, r)
+			return
+		}
+	}
+}
+
+func Method(m string) MiddleWare {
+	return func(handler http.HandlerFunc) http.HandlerFunc {
+		return func(writer http.ResponseWriter, request *http.Request) {
+			if request.Method != m {
+				http.Error(writer, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			}
+			handler(writer, request)
+		}
+	}
+}
+
+func Chain(f http.HandlerFunc, middlewares ...MiddleWare) http.HandlerFunc {
+	for _, m := range middlewares {
+		f = m(f)
+	}
+	return f
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintln(w, "hello world")
+}
+
+func main() {
+	http.HandleFunc("/", Chain(Hello, Method("GET"), Logging()))
+	_ = http.ListenAndServe(":8080", nil)
+}
